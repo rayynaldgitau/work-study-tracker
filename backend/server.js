ff273713b6cc6851fb21770908c24e1d31347f67
raynaldgitau <raynaldgitau@gmail.com>
2026-04-07 18:25:58 +0300

    fixing a server issue

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
 app.use(express.json({ limit: '1mb' }));
 
